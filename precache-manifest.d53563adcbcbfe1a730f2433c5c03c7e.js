71be5d1a96ba6a3ed5be725bc2ca313e299b65f6
Jan Karaczun <janek.karaczun@yahoo.com>
2019-07-05 16:15:19 +0200

    Updates

---
+++ precache-manifest.d53563adcbcbfe1a730f2433c5c03c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8453fe0055e690d7e54cf3d00a3976b",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "db2fdd90b9f78a6d1e4d",
+    "url": "/tictactoe/static/css/main.71d73fac.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tictactoe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "db2fdd90b9f78a6d1e4d",
+    "url": "/tictactoe/static/js/main.a03b568b.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
